50622f9921b75448602e333dcc6ca774153d2d43
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-02-09 21:54:20 -0500

    Set up generation for the command arguments

--- UnrealPluginManager.Cli.Generator/Generators/CommandGenerator.cs
+++ UnrealPluginManager.Cli.Generator/Generators/CommandGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.CodeDom.Compiler;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
 using Mustache;
 using UnrealPluginManager.Cli.Generator.Commands;
 using UnrealPluginManager.Cli.Generator.Utilities;
@@ -14,35 +16,67 @@ public class CommandGenerator : IIncrementalGenerator {
                 .Where(x => x.IsAbstract == false)
                 .Where(x => x.GetAttributes()
                     .Any(a => a.AttributeClass?.Name == nameof(CommandAttribute)))
-                .Select((x, _) => {
+                .Where(x => x
+                    .GetMembers()
+                    .Any(s => s.Kind == SymbolKind.Method && s is { IsStatic: false, IsAbstract: false, Name: "Execute" }))
+                .SelectMany((x, _) => {
                     var className = x.ToDisplayString();
                     var attribute = x.GetAttributes()
                         .First(a => a.AttributeClass?.Name == nameof(CommandAttribute));
                     var name = attribute.ConstructorArguments[0].Value!.ToString();
                     var description = attribute.ConstructorArguments[1].Value?.ToString();
+                    
+                     var executeMethod = x
+                         .GetMembers()
+                         .Where(s => s.Kind == SymbolKind.Method && s is { IsStatic: false, IsAbstract: false, Name: "Execute" })
+                         .Cast<IMethodSymbol>()
+                         .First();
 
-                    return new List<string> {
-                        $"var {name} = new {className}();",
-                        $"var {name}Command = new Command(\"{name}\", \"{description ?? ""}\");",
-                        $"{name}Command.SetHandler(() => {name}.Execute());",
-                        $"rootCommand.AddCommand({name}Command);"
-                    };
+                     var paramNames = string.Join(", ", executeMethod.Parameters
+                         .Select(p => p.Name));
+                     var joinedOptions = string.Join(", ", executeMethod.Parameters
+                         .Select((p, i) => $"{name}Option{i}"));
+
+                     var parameters = executeMethod.Parameters
+                         .SelectMany((p, i) => {
+                             var type = p.Type.ToDisplayString();
+                             var param = Regex.Replace(p.Name, "(?<!^)([A-Z])", "-$1").ToLower();
+
+                             return new [] {
+                                $"var {name}Option{i} = new Option<{type}>(\"--{param}\");",
+                                $"{name}Command.AddOption({name}Option{i});"
+                             };
+                         });
+
+                     return new[] {
+                             $"var {name} = new {className}();",
+                             $"var {name}Command = new Command(\"{name}\", \"{description ?? ""}\");",
+                         }.Concat(parameters)
+                         .Concat([
+                             $"{name}Command.SetHandler(({paramNames}) => {name}.Execute({paramNames}){(joinedOptions.Length > 0 ? $", {joinedOptions}" : "")});",
+                             $"rootCommand.AddCommand({name}Command);"
+                         ]);
                 }));
 
         context.RegisterSourceOutput(selected, (ctx, data) => {
-            var start = """
-                        using System.CommandLine;
-
-                        namespace UnrealPluginManager.Cli.Utils;
-
-                        public  static partial class CommandUtils {
-                        
-                         public static partial void SetUpCommands(this RootCommand rootCommand) {
-                        """;
-
-            var end = "} }";
-            var result = string.Join('\n', data.SelectMany(x => x));
-            ctx.AddSource("CommandUtils.g.cs", $"{start}{result}{end}");
+            var textWriter = new StringWriter();
+            var writer = new IndentedTextWriter(textWriter);
+            writer.WriteLine("using System.CommandLine;");
+            writer.WriteLine();
+            writer.WriteLine("namespace UnrealPluginManager.Cli.Utils;");
+            writer.WriteLine();
+            writer.WriteLine("public static partial class CommandUtils {");
+            writer.Indent++;
+            writer.WriteLine("public static partial void SetUpCommands(this RootCommand rootCommand) {");
+            writer.Indent++;
+            foreach (var line in data) {
+                writer.WriteLine(line);
+            }
+            writer.Indent--;
+            writer.WriteLine("}");
+            writer.Indent--;
+            writer.WriteLine("}");
+            ctx.AddSource("CommandUtils.g.cs", textWriter.ToString());
         });
     }
 }
--- UnrealPluginManager.Cli/Program.cs
+++ UnrealPluginManager.Cli/Program.cs
@@ -3,3 +3,5 @@ using UnrealPluginManager.Cli.Utils;
 
 var rootCommand = new RootCommand();
 rootCommand.SetUpCommands();
+
+var opt = new Option<string>("--engine-path");
