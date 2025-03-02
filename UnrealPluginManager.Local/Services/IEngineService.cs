97042afe72d31a24345c38efd41de38bb49e423c
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-02 17:10:56 -0500

    Added a method to get all the installed versions of the engine (#38)
    * Added a method to get all the installed versions of the engine
    
    * Removed a type
    
    * Revert "Removed a type"
    
    This reverts commit 155cf4754f6108c4163edaee452253308799b3e1.

--- UnrealPluginManager.Local/Services/IEngineService.cs
+++ UnrealPluginManager.Local/Services/IEngineService.cs
@@ -6,6 +6,11 @@ using UnrealPluginManager.Local.Model.Engine;
 namespace UnrealPluginManager.Local.Services;
 
 /// <summary>
+/// Represents an installed Unreal Engine plugin with its name and version details.
+/// </summary>
+public record struct InstalledPlugin(string Name, SemVersion Version);
+
+/// <summary>
 /// Provides functionalities related to engine management within the Unreal Plugin Manager CLI.
 /// </summary>
 public interface IEngineService {
@@ -30,8 +35,32 @@ public interface IEngineService {
     /// A task representing the asynchronous operation with an integer exit code indicating the result of the build process.
     /// </returns>
     public Task<int> BuildPlugin(IFileInfo pluginFile, string? engineVersion);
-    
+
+    /// <summary>
+    /// Retrieves the installed version of a specified plugin for a given Unreal Engine version, if available.
+    /// </summary>
+    /// <param name="pluginName">
+    /// The name of the plugin for which the version is being retrieved.
+    /// </param>
+    /// <param name="engineVersion">
+    /// The version of the Unreal Engine to check for the installed plugin. This parameter can be null.
+    /// </param>
+    /// <returns>
+    /// An <see cref="Option{T}"/> containing the installed <see cref="SemVersion"/> of the plugin if found; otherwise, an empty option.
+    /// </returns>
     public Task<Option<SemVersion>> GetInstalledPluginVersion(string pluginName, string? engineVersion);
+
+    /// <summary>
+    /// Retrieves a list of installed plugins for a specified Unreal Engine version.
+    /// </summary>
+    /// <param name="engineVersion">
+    /// The version of the Unreal Engine for which to retrieve the installed plugins.
+    /// If null, the default engine version will be used.
+    /// </param>
+    /// <returns>
+    /// An asynchronous enumerable of <see cref="InstalledPlugin"/> objects representing the plugins installed for the specified engine version.
+    /// </returns>
+    public IAsyncEnumerable<InstalledPlugin> GetInstalledPlugins(string? engineVersion);
 
     /// <summary>
     /// Installs the specified plugin for a given Unreal Engine version.
