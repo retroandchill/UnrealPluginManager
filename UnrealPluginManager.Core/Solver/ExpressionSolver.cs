c8bd2d3cb99c528e41231e2475c4dd2d41709d23
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-02 16:28:34 -0500

    Order by dependency frequency to sus out potential conflicts and fail faster if not solution exists

--- UnrealPluginManager.Core/Solver/ExpressionSolver.cs
+++ UnrealPluginManager.Core/Solver/ExpressionSolver.cs
@@ -75,8 +75,17 @@ public static class ExpressionSolver {
     public static IExpression Convert<T>(string root, SemVersion rootVersion, IDictionary<string, T> pluginData)
         where T : IEnumerable<IDependencyChainNode> {
         List<IExpression> terms = [new Var(new SelectedVersion(root, rootVersion))];
-        foreach (var pack in
-                 pluginData.Values.SelectMany(x => x.OrderBy(y => y.Version, SemVersion.PrecedenceComparer))) {
+
+        var dependencyFrequency = pluginData.SelectMany(x => x.Value)
+                .SelectMany(x => x.Dependencies)
+                .GroupBy(x => x.PluginName)
+                .Select(x => (x.Key, x.Count()))
+                .OrderByDescending(x => x.Item2)
+                .Select(x => x.Item1)
+                .ToList();
+        
+        foreach (var pack in dependencyFrequency.Select(x => pluginData[x])
+                         .SelectMany(x => x.OrderBy(y => y.Version, SemVersion.PrecedenceComparer))) {
             terms.AddRange(pack.Dependencies.Where(dep => dep.Type == PluginType.Provided)
                 .Select(dep => pluginData[dep.PluginName]
                     .Where(pd => dep.PluginVersion.Contains(pd.Version))
