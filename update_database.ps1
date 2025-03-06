d4154d4eab83fa8cd6473de6ef773f8a4599e5ac
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-06 18:01:32 -0500

    Updated the other project

---
+++ update_database.ps1
@@ -0,0 +1,12 @@
+﻿$current_directory = Get-Location
+try {
+    Set-Location UnrealPluginManager.Server
+    dotnet ef migrations add $args
+    dotnet ef database update
+
+    Set-Location ../UnrealPluginManager.Cli
+    dotnet ef migrations add $args --project ../UnrealPluginManager.Local
+    dotnet ef database update --project ../UnrealPluginManager.Local
+} finally {
+    Set-Location $current_directory
+}
