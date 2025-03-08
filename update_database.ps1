8aa3bd222dbb750bb25b2516b47ebb69410c90a3
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-08 11:23:11 -0500

    Rearranged the project directories (#62)
    * Changed the solution structure
    
    * Changed the entire project structure to be a bit more organized

--- update_database.ps1
+++ update_database.ps1
@@ -1,10 +1,10 @@
 ﻿$current_directory = Get-Location
 try {
-    Set-Location UnrealPluginManager.Server
+    Set-Location UnrealPluginManager.Server/Source/UnrealPluginManager.Server
     dotnet ef migrations add $args
     dotnet ef database update
 
-    Set-Location ../UnrealPluginManager.Cli
+    Set-Location ../../../UnrealPluginManager.Local/Source/UnrealPluginManager.Cli
     dotnet ef migrations add $args --project ../UnrealPluginManager.Local
     dotnet ef database update --project ../UnrealPluginManager.Local
 } finally {
