18614f06e351613dac0399b3897a5b6964f9116d
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-18 17:21:37 -0400

    Stubbed out the plugin page

---
+++ unrealpluginmanager.client/src/components/PluginPage.tsx
@@ -0,0 +1,23 @@
+﻿import {useEffect, useState} from 'react';
+import Markdown from 'react-markdown'
+import {PluginVersionInfo} from "@/api";
+import {pluginsApi} from "@/config";
+
+interface PluginPageProps {
+  plugin: PluginVersionInfo
+}
+
+function PluginPage({ plugin } : Readonly<PluginPageProps>) {
+  const [readme, setReadme] = useState("");
+  
+  useEffect(() => {
+    pluginsApi.getPluginReadme({pluginId: plugin.pluginId, versionId: plugin.versionId})
+        .then((text) => setReadme(text))
+  });
+  
+  return (
+      <Markdown>{readme}</Markdown>
+  );
+}
+
+export default PluginPage;
