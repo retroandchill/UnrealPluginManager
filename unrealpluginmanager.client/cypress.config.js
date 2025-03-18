6e0748c3c238079442d0e2e63c9f1b02ac423efa
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-17 22:15:11 -0400

    Generate a separate directory for each lcov file

--- unrealpluginmanager.client/cypress.config.js
+++ unrealpluginmanager.client/cypress.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "cypress";
 import codeCoverageTask from "@cypress/code-coverage/task";
 import useBabelRc from '@cypress/code-coverage/use-babelrc';
 import path from 'path';
+import * as fs from 'fs';
 const __dirname = import.meta.dirname;
 
 export default defineConfig({
@@ -13,6 +14,13 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       codeCoverageTask(on, config);
       on('file:preprocessor', useBabelRc)
+
+      on("after:spec", async (spec, results) => {
+        const specName = path.basename(spec.name, path.extname(spec.name)); // Get spec name (without extension)
+        const outputDir = path.join(__dirname, "coverage");
+        fs.rename(outputDir, path.join(__dirname, `coverage-${specName}`))
+      });
+      
       return config;
     },
   },
