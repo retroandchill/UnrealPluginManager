0625c0ce9f876aaaf99754c9556c56675d83aba4
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-04-06 14:40:12 -0400

    Added a custom defined authorization policy

---
+++ UnrealPluginManager.Server/Source/UnrealPluginManager.Server/Auth/AuthorizationPolicies.cs
@@ -0,0 +1,18 @@
+﻿namespace UnrealPluginManager.Server.Auth;
+
+/// <summary>
+/// Provides a collection of constants representing the authorization policies used within the application.
+/// </summary>
+/// <remarks>
+/// The policies defined in this class are utilized to categorize and enforce access control for various operations.
+/// </remarks>
+public static class AuthorizationPolicies {
+  /// <summary>
+  /// Defines an authorization policy specific to contributors who manage plugins within the system.
+  /// </summary>
+  /// <remarks>
+  /// This policy is used to restrict access to operations that involve submitting, adding, or modifying plugin details,
+  /// ensuring that only authorized contributors have the necessary permissions to perform such actions.
+  /// </remarks>
+  public const string PluginContributors = "PluginContributors";
+}
