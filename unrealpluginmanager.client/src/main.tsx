bad3d0c31d490ea482d4d3ac1bc8ed50ae6fe68f
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-04-19 15:53:55 -0400

    Attempt to pull in a landing page theme

--- unrealpluginmanager.client/src/main.tsx
+++ unrealpluginmanager.client/src/main.tsx
@@ -1,19 +1,8 @@
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import {createBrowserRouter} from "react-router-dom";
-import CssBaseline from '@mui/material/CssBaseline';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 
 createRoot(document.getElementById('root')!).render(
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-      <App routerFactory={createBrowserRouter}/>
-    </ThemeProvider>
+    <App routerFactory={createBrowserRouter}/>
 )
