dd37b716357cf2f7cf934313261fa5eb676ecc53
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-05-11 20:19:49 -0400

    Revert "Add another dep to optimize"
    This reverts commit 2aa2281925287800e9975bb6ea9e4fc76c5389e5.

--- unrealpluginmanager.client/vite.config.ts
+++ unrealpluginmanager.client/vite.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     entries: ['cypress/**/*'],
-    include: ['react-router', 'oidc-client-ts', 'uuid']
+    include: ['react-router', 'oidc-client-ts']
   },
   resolve: {
     alias: {
